33c77a2f9fb621b9d5d74cbb0e8a0004184d7519
Sudhindra <sudheendrapai.b@gmail.com>
2020-12-15 00:37:14 +0530

    Modifications as per #7

--- livestreams/views.py
+++ livestreams/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Creating Views here (soon will be changed to class based-views).
 
-def livestreams(request):
-    return render(request, 'livestreams/livestreams.html')
+def liveEvents(request):
+    return render(request, 'livestreams/live_event.html')
