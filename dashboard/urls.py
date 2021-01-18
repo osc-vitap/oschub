94269b5e5565c3f83989c5d8472b4a22018d0d1e
DASARI BHARGAV <bhargav87654322@gmail.com>
2021-01-18 12:56:07 +0530

    settings.py is updated with all requirement for allauth login main.html is updated with basic login/logout process url.py is updated to redirect to allauth

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard')
+    path('', views.dashboard, name='dashboard'),
+    path('accounts/', include('allauth.urls')),
 ]
