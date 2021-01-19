055c42332e487757678c2fcfe66e967eecb31873
kode-logger <nkrishnaraj0907@gmail.com>
2021-01-19 18:34:52 +0530

    This commit fixes #12  Following files have been changed: 	+ eventreg/admin.py 	+ eventreg/models.py 	+ Added migration file  Following are the tasks completed:  	* Created model class  	* Defined the model class EventData  	* Added the model to the admin site  	* Migrated the model as database

--- eventreg/models.py
+++ eventreg/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class EventData(models.Model):
+    eventName = models.CharField(max_length=264)
+    eventDescription = models.TextField()
+    eventVenue = models.CharField(max_length=50)
+    eventDate = models.DateField()
+    eventTime = models.TimeField()
+    eventSpeaker = models.CharField(max_length=50)
+    numAttendees = models.IntegerField()
