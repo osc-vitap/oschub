0a8f620852a0b65689963c0bad719b682a6c8bc1
DASARI BHARGAV <bhargav87654322@gmail.com>
2021-01-21 16:20:39 +0530

    in eventreg app in views.py added class based views (listview and detailview) renamed event_list.html to eventdata_list.html eventdata_list.html displays list of events event_detail.html displays event details url.py is modified to handle class based views
    in dashboard
    in url.py removed allauth redirection
    main.html now shows profile picture

--- eventreg/admin.py
+++ eventreg/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import EventData
+
 # Register your models here.
 
 admin.site.register(EventData)
--- eventreg/models.py
+++ eventreg/models.py
@@ -3,9 +3,10 @@ from django.db import models
 
 # Create your models here.
 class EventData(models.Model):
-    eventName = models.CharField(max_length=264, primary_key=True)
+    eventName = models.CharField(max_length=264)
     eventDescription = models.TextField()
     eventVenue = models.CharField(max_length=50)
-    eventDate = models.DateField(editable=True)
-    eventTime = models.TimeField(editable=True)
-    eventSpeaker = models.TextField(editable=True)
+    eventDate = models.DateField()
+    eventTime = models.TimeField()
+    eventSpeaker = models.CharField(max_length=50)
+    numAttendees = models.IntegerField()
