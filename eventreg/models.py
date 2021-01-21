bb8afc29fa536d766fde0d7f22eadaf4671fde64
kode-logger <nkrishnaraj0907@gmail.com>
2021-01-21 19:34:48 +0530

    Fixed a major code-breaking bug. Added default 'id' value which was removed in the previous commit by mistake.

--- eventreg/models.py
+++ eventreg/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class EventData(models.Model):
-    eventName = models.CharField(max_length=264, primary_key=True)
+    eventName = models.CharField(max_length=264)
     eventDescription = models.TextField()
     eventVenue = models.CharField(max_length=50)
     eventDate = models.DateField(editable=True)
