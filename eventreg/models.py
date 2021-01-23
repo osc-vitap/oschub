19b3d3bad7773b56e37220cab6612c126c945e1d
kode-logger <nkrishnaraj0907@gmail.com>
2021-01-23 17:19:29 +0530

    [O] Update Log: 	+ Added new EventUserData model to Admin and Added Admin model for EventUserData to enable search filters and display view 	+ Added new EventUserData model that will store the details of a student registering for an event along with the eventname 	+ Migrated all changes made to the databases

--- eventreg/models.py
+++ eventreg/models.py
@@ -9,6 +9,19 @@ class EventData(models.Model):
     eventDate = models.DateField(editable=True)
     eventStartTime = models.TimeField(editable=True)
     eventEndTime = models.TimeField(editable=True)
+    eventRegEndDate = models.DateField(editable=True)
+    eventRegEndTime = models.DateField(editable=True)
     eventSpeaker = models.TextField(editable=True)
     eventURL = models.URLField(editable=True)
-# class EventUserData(models.Model):
+
+    def __str__(self):
+        return str(self.eventName)
+
+
+class EventUserData(models.Model):
+    eventName = models.ForeignKey(EventData, on_delete=models.CASCADE)
+    studentName = models.CharField(max_length=264, unique=True)
+    studentReg = models.CharField(max_length=10, unique=True)
+    studentEmail = models.EmailField(unique=True)
+    studentRegistered = models.BooleanField(default=False)
+    studentCheckedIn = models.BooleanField(default=False)
