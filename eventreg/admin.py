d10ad0255af5e8ee7e2a9cf10c3dcd68b756b681
kode-logger <nkrishnaraj0907@gmail.com>
2021-01-23 17:19:29 +0530

    [O] Update Log: 	+ Added new EventUserData model to Admin and Added Admin model for EventUserData to enable search filters and display view 	+ Added new EventUserData model that will store the details of a student registering for an event along with the eventname 	+ Migrated all changes made to the databases

--- eventreg/admin.py
+++ eventreg/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import EventData
+from .models import EventData, EventUserData
 
 
 class EventAdmin(admin.ModelAdmin):
@@ -7,4 +7,10 @@ class EventAdmin(admin.ModelAdmin):
     list_display = ['eventName', 'eventDate', 'eventStartTime', 'eventEndTime', 'eventSpeaker']
 
 
+class EventDataAdmin(admin.ModelAdmin):
+    search_fields = ['eventName', 'studentName', 'studentReg', 'studentEmail']
+    list_display = ['eventName', 'studentReg', 'studentName']
+
+
 admin.site.register(EventData, EventAdmin)
+admin.site.register(EventUserData, EventDataAdmin)
