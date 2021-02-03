d71322b70226df5ca8880ed7ad792dcd791f179e
kode-logger <nkrishnaraj0907@gmail.com>
2021-02-04 00:31:08 +0530

    [#] Reformatted settings.py and oschub/urls.py.

--- oschub/urls.py
+++ oschub/urls.py
@@ -5,10 +5,15 @@ from django.conf.urls.static import static
 
 from . import views
 
-urlpatterns = [path('admin/', admin.site.urls), path('', include('accounts.urls')),
-    path('accounts/', include('allauth.urls')), path('dashboard/', include('dashboard.urls')),
-    path('eventreg/', include('eventreg.urls')), path('livestream/', include('livestreams.urls')),
-    path('admin/updateSheet', views.updateSheets, name='updateSheets'), ]
+urlpatterns = [
+        path('admin/', admin.site.urls),
+        path('', include('accounts.urls')),
+        path('accounts/', include('allauth.urls')),
+        path('dashboard/', include('dashboard.urls')),
+        path('eventreg/', include('eventreg.urls')),
+        path('livestream/', include('livestreams.urls')),
+        path('admin/updateSheet', views.updateSheets, name='updateSheets'),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
