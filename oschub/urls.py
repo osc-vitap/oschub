4a09efbf1509fdda7485f0b445cfd3083722ffb5
kode-logger <nkrishnaraj0907@gmail.com>
2021-02-04 09:47:21 +0530

    [#] Re-Reformatted the requested files.  [#] Migrated from Function Based View to Class Based View  [#] Added pillow package to requirements.txt to store images for Event model

--- oschub/urls.py
+++ oschub/urls.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
-from . import views
+from .views import UpdateSheet
 
 urlpatterns = [
         path('admin/', admin.site.urls),
@@ -12,7 +11,7 @@ urlpatterns = [
         path('dashboard/', include('dashboard.urls')),
         path('eventreg/', include('eventreg.urls')),
         path('livestream/', include('livestreams.urls')),
-        path('admin/updateSheet', views.updateSheets, name='updateSheets'),
+        path('admin/updateSheet', UpdateSheet.as_view(), name='updateSheets'),
 ]
 
 if settings.DEBUG:
