9c359168fe12b418c1d8b64b6d4c31caca9e5cfb
DASARI BHARGAV <bhargav87654322@gmail.com>
2021-02-12 01:43:24 +0530

    event_detail.html - if the user is anonymous the register button will not be visible - if the user is anonymous LOGIN TO REGISTER hyper link will be visible - if the user is logged in and not registered the register button will be displayed - if the user is logged in and register then hyper link to livestream will be displayed

--- eventreg/views.py
+++ eventreg/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import redirect, render
+from django.template import RequestContext
+from django.urls import reverse
 from django.views.generic import (ListView, DetailView)
 from django.http import HttpResponseRedirect
 from eventreg.models import EventUserData, Event
@@ -13,19 +16,29 @@ class EventDetailView(DetailView):
     model = Event
     template_name = 'eventreg/event_detail.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
-
-    def post(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         if request.user.is_anonymous:
             return HttpResponseRedirect("/accounts/google/login/")
+        a = EventUserData.objects.filter(eventName=kwargs['pk'], studentEmail=request.user.email)
+        if len(a) > 0:
+            if a[0].studentRegistered:
+                eve = True
+            else:
+                eve = False
+        else:
+            eve = False
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        context["is_stu_registered"] = eve
+        return self.render_to_response(context)
 
+    def post(self, request, *args, **kwargs):
         user_name = request.user.first_name.title()
         user_email = request.user.email
         user_reg = user_email.split('.')[1].split('@')[0].upper()
-        event_name = request.POST.get("eventname", "")
-        eventdata_instance = EventUserData.objects.create(eventName=Event.objects.get(eventName=event_name),
+        a = EventUserData.objects.filter(eventName=kwargs['pk'], studentEmail=request.user.email)
+        if len(a) ==0:
+            eventdata_instance = EventUserData.objects.create(eventName=Event.objects.get(**kwargs),
                                                           studentName=user_name,
                                                           studentReg=user_reg,
                                                           studentEmail=user_email,
