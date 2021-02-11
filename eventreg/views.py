0e456a92940d0534da4f9e14f2895497824ad1d1
DASARI BHARGAV <bhargav87654322@gmail.com>
2021-02-12 01:56:54 +0530

    major update views.py - will now prevent multiple registrations for same event - update the context variable is_stu_registered with true or false - true if the student is registered and if not - it will be used in template tag to display live stream line if true ,if not register button
    event_detail.html
    - if the user is anonymous the register button will not be visible
    - if the user is anonymous LOGIN TO REGISTER hyper link will be visible
    - if the user is logged in and not registered the register button will be displayed
    - if the user is logged in and register then hyper link to livestream will be displayed

--- eventreg/views.py
+++ eventreg/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import redirect, render
-from django.template import RequestContext
-from django.urls import reverse
 from django.views.generic import (ListView, DetailView)
 from django.http import HttpResponseRedirect
 from eventreg.models import EventUserData, Event
@@ -51,4 +48,3 @@ class LiveStreamView(DetailView):
     context_object_name = "event"
     template_name = "eventreg/live_event.html"
     model = Event
-
