e2871ca24f9922628d27df346b17ab7ce4587c61
kode-logger <nkrishnaraj0907@gmail.com>
2021-02-18 21:34:13 +0530

    + Added Speaker model to Dashboard Application. + Migrated Speaker model to DataBase successfully. - Updated the eventreg/migrations/0001_initial.py as it was outdated, causing Operational Error.

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Speaker(models.Model):
+    speakerName = models.CharField(max_length=64, editable=True)
+    speakerProfession = models.CharField(max_length=64, editable=True)
+    speakerImage = models.URLField(editable=True)
