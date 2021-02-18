abdb296fb7c154356055888888530691d966650f
kode-logger <nkrishnaraj0907@gmail.com>
2021-02-18 16:12:53 +0530

    Minor Updates: 	+ Created a new method in SheetMe.py to get the list of events that are over 	+ Completed PopulationScript that generates n number of student data with the events in the database which are not over.

--- PopulateMe.py
+++ PopulateMe.py
@@ -8,6 +8,8 @@ django.setup()
 
 from faker import Faker
 import random
+from eventreg.models import Event, EventUserData
+from SheetMe import getCompletedEvents
 
 
 def genReg(num=1000):
@@ -24,20 +26,34 @@ def genReg(num=1000):
 def populateEventUserData(num=1000):
     fake = Faker()
 
-    nameList = [fake.first_name() for i in range(num + 1)]
+    nameList = [fake.name() for i in range(num + 1)]
     regList = genReg()
     # Email Generation
     emailList = []
     for i in range(num):
-        emailList.append(nameList[i].lower() + '.' + regList[i].lower() + '@vitap.ac.in')
+        emailList.append(nameList[i].split(' ')[0].lower() + '.' + regList[i].lower() + '@vitap.ac.in')
     # Student Data List generation
     studentData = []
-    for i in range(num):
-        student = [nameList[i], regList[i], emailList[i]]
-        studentData.append(student)
+    eventList = []
+    completedEvents = getCompletedEvents()
+    events = Event.objects.all()
+
+    for event in events:
+        eventList.append(event.eventName)
+
+    for event in [temp for temp in eventList if temp not in completedEvents]:
+        for eventData in Event.objects.filter(eventName=event):
+            for i in range(num):
+                student = [eventData, nameList[i], regList[i], emailList[i], True, random.choice([True, False])]
+                studentData.append(student)
 
     return studentData
 
 
 if __name__ == '__main__':
-    print(populateEventUserData())
+    modelData = populateEventUserData(500)
+    for data in modelData:
+        row = EventUserData(eventName=data[0], studentName=data[1], studentReg=data[2], studentEmail=data[3], studentRegistered=data[4], studentCheckedIn=data[5])
+        row.save()
+    print("Added data, check database.")
+
