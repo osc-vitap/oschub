3d64f0f8c7522885d44b597dafa8224cd7293c59
DASARI BHARGAV <bhargav87654322@gmail.com>
2021-02-25 01:18:51 +0530

    minor update in profile view
    views.py
    - now redirect the user to login if not logged in

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,15 @@
+from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
 
 
 class Profile(TemplateView):
     template_name = 'accounts/userprofile.html'
+    def get(self, request, *args, **kwargs):
+        if request.user.is_anonymous:
+            return HttpResponseRedirect("/accounts/google/login/")
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
 
 
 
