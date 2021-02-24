e28082cb58f1f15780ae1bd8810203b06aa38601
DASARI BHARGAV <bhargav87654322@gmail.com>
2021-02-25 01:14:38 +0530

    speaker backend
    admin.py of dashboard
    -added speaker module to admin
    
    models.py
    - add get_speakerImage to model.py to refactor and get the speakerImage
    
    views.py
    -changed SpeakerView to a listview from template view
    
    speakers.html
    -displays the speakers

--- dashboard/models.py
+++ dashboard/models.py
@@ -5,3 +5,6 @@ class Speaker(models.Model):
     speakerName = models.CharField(max_length=64, editable=True)
     speakerProfession = models.CharField(max_length=64, editable=True)
     speakerImage = models.URLField(editable=True)
+
+    def get_speakerImage (self):
+        return "https://drive.google.com/uc?export=view&id={}".format(str(self.speakerImage.split('/')[5]))
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-
+from django.views.generic import TemplateView,ListView
+from dashboard.models import Speaker
 
 class DashboardView(TemplateView):
     template_name = "dashboard/main.html"
 
-
-class SpeakerView(TemplateView):
+class SpeakerView(ListView):
     template_name = "dashboard/speakers.html"
-
+    context_object_name = 'Speakers'
+    model = Speaker
 
 class AboutView(TemplateView):
     template_name = "dashboard/about.html"
