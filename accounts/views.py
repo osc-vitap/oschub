dc69cc71f637d43c0b0f0c16a5cef8b2794bbf1d
DASARI BHARGAV <bhargav87654322@gmail.com>
2021-02-26 02:26:53 +0530

    update in accounts views.py - now profile view checks if the user is logged in or not - if logged in it displays the profile - if not it redirects to login page
     urls.py
     -login/ is removed
     -now by default it directs to login view

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
@@ -7,11 +8,8 @@ class Profile(TemplateView):
     def get(self, request, *args, **kwargs):
         if request.user.is_anonymous:
             return HttpResponseRedirect("/accounts/google/login/")
-        self.object = self.get_object()
-        context = self.get_context_data(object=self.object)
-        return self.render_to_response(context)
 
-
+        return render(request, self.template_name)
 
 class Login(TemplateView):
     template_name = 'accounts/login.html'
