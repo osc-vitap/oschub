b7437f5ffcc487fa2c28549f6f465d7e88c764bb
DASARI BHARGAV <bhargav87654322@gmail.com>
2021-02-26 02:21:38 +0530

    upcoming,current,past dashboard update views.py -added get method -it checks for user login, if not it redirects to login - it filter all the event user has registered in to Quary -from this Quary the events are sorted into three category -category 1 upcoming event -if the event is scheduled any day after today -category 2 current event -if the event is scheduled today -category 3 past event -if the event is scheduled any day before today -after sorting it added to context variable respectively
    main.html
    - updated to display upcoming,current,past events
    
    commit to issue  #39

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,39 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView,ListView
+from django.http import HttpResponseRedirect
+from django.views.generic import TemplateView, ListView
+from django.views.generic.detail import SingleObjectMixin
 from dashboard.models import Speaker
+from eventreg.models import EventUserData, Event
+import datetime
 
 class DashboardView(TemplateView):
     template_name = "dashboard/main.html"
+
+    def get_object(self):
+        return self
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_anonymous:
+            return HttpResponseRedirect("/accounts/google/login/")
+        Quary = EventUserData.objects.filter(studentEmail=request.user.email)
+        upcoming=[]
+        current=[]
+        past=[]
+        if len(Quary) > 0:
+            for i in Quary:
+                Quary1=Event.objects.get(eventName=i.eventName)
+                if Quary1.eventDate > datetime.date.today():
+                    upcoming.append(Quary1)
+                elif Quary1.eventDate < datetime.date.today():
+                    past.append(Quary1)
+                else:
+                    current.append(Quary1)
+
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        context["upcoming"] = upcoming
+        context["current"] = current
+        context["past"] = past
+        return self.render_to_response(context)
 
 class SpeakerView(ListView):
     template_name = "dashboard/speakers.html"
