ae5a8d2c6b4bbafa973cc4b1ee36de428c92cb6c
dat-adi <osc@vitap.ac.in>
2021-02-28 15:48:13 +0530

    Updated the name and the migrations.

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from .models import MailList
 
 
-# Register your models here.
-
-
 class MailListAdmin(admin.ModelAdmin):
     search_fields = ["email"]
     list_display = ["email"]
