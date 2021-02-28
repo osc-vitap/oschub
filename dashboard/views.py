557fe6d157e2f3dabfb3fb226560ab3f9e21fee9
KodeLogger <nkrishnaraj0907@gmail.com>
2021-02-28 20:39:55 +0530

    Finalized the working of Feedback Form 	- Added post method to send the feedback mail (views.py) 	- Configured smtp server to send emails (settings.py)

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView, ListView
 from dashboard.models import Speaker
 from eventreg.models import EventUserData, Event
+from django.core.mail import send_mail  # used to send mail (feedback form_
 import datetime
 
 
@@ -44,3 +45,11 @@ class SpeakerView(ListView):
 
 class AboutView(TemplateView):
     template_name = "dashboard/about.html"
+
+    def post(self, request):
+        emailto = ''  # Put The Receiver's Email Id here
+        msg = request.POST[
+                  "message"] + f"\n\nName of Sender: {request.POST['name']} \nEmail of Sender: {request.POST['email']}"
+        send_mail(subject=f"OSCHub Feedback from {request.POST['name']}", message=msg,
+                  from_email=request.POST["email"], recipient_list=[emailto])
+        return HttpResponseRedirect(request.META.get("HTTP_REFERER", "/"))
