22c2084518c6f4844a38921194962a9ba1d4933a
DASARI BHARGAV <bhargav87654322@gmail.com>
2021-02-28 21:31:42 +0530

    completed userprofile stats - views.py - removed previous context variables - add three new variables - stat1 % of event registered but not attended - stat2 % of event registered and attended - stat3 % of event not registered and not attended

--- accounts/views.py
+++ accounts/views.py
@@ -18,26 +18,13 @@ class Profile(TemplateView):
         context = self.get_context_data(object=self.object)
 
         Quary1 = EventUserData.objects.filter(studentEmail=request.user.email)
-        all = []
-        attended = []
-        not_attended = []
-        for i in Quary1:
-            Quary2 = Event.objects.get(eventName=i.eventName)
-            all.append({"eventName": i.eventName, "eventDate": Quary2.eventDate})
-            if i.studentCheckedIn:
-                attended.append(
-                    {"eventName": i.eventName, "eventDate": Quary2.eventDate}
-                )
-            else:
-                not_attended.append(
-                    {"eventName": i.eventName, "eventDate": Quary2.eventDate}
-                )
-        context["events"] = all
-        context["events_attended"] = attended
-        context["event_not_attended"] = not_attended
-        context["total_events"] = len(all)
-        context["total_events_attended"] = len(attended)
-        context["total_event_not_attended"] = len(not_attended)
+        Quary2 = EventUserData.objects.filter(studentEmail=request.user.email,studentCheckedIn=True)
+        Quary3 = EventUserData.objects.filter(studentEmail=request.user.email, studentCheckedIn=False)
+        Quary4 = Event.objects.all()
+
+        context["stat1"] = (len(Quary3)/len(Quary1))*100
+        context["stat2"] = (len(Quary2)/len(Quary1))*100
+        context["stat3"] = (len(Quary3)/len(Quary4))*100
         context["user_regno"] = request.user.email.split(".")[1].split("@")[0].upper()
         return self.render_to_response(context)
 
