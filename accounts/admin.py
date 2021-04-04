62fd2ce22c71f8225e493d95ef6b604897e47f73
dat-adi <dat.adithya@gmail.com>
2021-04-05 00:26:16 +0530

    testing

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from .models import MailList
 
 
+'''
 class MailListAdmin(admin.ModelAdmin):
     search_fields = ["email"]
     list_display = ["email"]
 
 
 admin.site.register(MailList, MailListAdmin)
+'''
--- oschub/settings.py
+++ oschub/settings.py
@@ -138,6 +138,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIR = (
+        os.path.join(BASE_DIR, 'static'),
+    )
 
 SITE_ID = 1
 ACCOUNT_LOGOUT_ON_GET = True
