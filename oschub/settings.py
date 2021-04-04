075e71c392287e457d9bf802e3335465076bcf52
Dat Adithya <31721284+dat-adi@users.noreply.github.com>
2021-04-04 23:42:17 +0530

    Added STATIC_ROOT

--- oschub/settings.py
+++ oschub/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 SITE_ID = 1
 ACCOUNT_LOGOUT_ON_GET = True
