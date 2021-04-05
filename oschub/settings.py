e0da91a905e6ebacbb65eb50b659e700da83c209
dat-adi <dat.adithya@gmail.com>
2021-04-05 16:29:57 +0530

    Removal of redundant modules

--- oschub/settings.py
+++ oschub/settings.py
@@ -1,12 +1,8 @@
-from dotenv import load_dotenv
 from pathlib import Path
 from decouple import config, Csv
 import os
 import dj_database_url
 
-load_dotenv()
-env_path = Path(".") / ".env"
-load_dotenv(dotenv_path=env_path)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
