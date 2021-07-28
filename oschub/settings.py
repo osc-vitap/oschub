77850adb54d2b2f2319b359e4c5fb984f66e5776
KodeLogger <nkrishnaraj0907@gmail.com>
2021-07-28 13:28:13 +0530

    Reordered lines, partially fixes SMTP AuthError

--- oschub/settings.py
+++ oschub/settings.py
@@ -168,6 +168,6 @@ LOGIN_REDIRECT_URL = "/dashboard/"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
 EMAIL_HOST_USER = config('USER_EMAIL')
 EMAIL_HOST_PASSWORD = config('USER_PASS')
+EMAIL_PORT = 587
