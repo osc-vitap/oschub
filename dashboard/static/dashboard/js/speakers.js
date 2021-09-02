00ab7002be088c094f5ddb742d41240662b314c0
Mahesh Dasika <dasikamahesh1@gmail.com>
2021-09-02 22:05:30 +0530

    updated speaker section to support mobile view

--- dashboard/static/dashboard/js/speakers.js
+++ dashboard/static/dashboard/js/speakers.js
@@ -4,4 +4,23 @@ $(".slider").owlCarousel({
   autoplayTimeout: 2000,
   autoplayHoverPause: true,
   dots:true,
+  responsiveClass:true,
+        responsive:
+        {
+          0:
+          {
+            items:1,
+            nav:false,
+          },
+          600:
+          {
+            items:1,
+            nav:false,
+          },
+          1000:
+          {
+            items:3,
+            nav:false,
+          }
+        }
 })
