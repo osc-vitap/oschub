cece5edec82b223e7d57051a6531594525e10215
mahesh-11102 <77436328+mahesh-11102@users.noreply.github.com>
2021-09-07 00:14:07 +0530

    Update speakers.js

--- dashboard/static/dashboard/js/speakers.js
+++ dashboard/static/dashboard/js/speakers.js
@@ -5,22 +5,15 @@ $(".slider").owlCarousel({
   autoplayHoverPause: true,
   dots:true,
   responsiveClass:true,
-        responsive:
-        {
-          0:
-          {
-            items:1,
-            nav:false,
-          },
-          600:
-          {
-            items:1,
-            nav:false,
-          },
-          1000:
-          {
-            items:3,
-            nav:false,
-          }
-        }
+  responsive:
+  {
+    0:
+    {
+      items:1,
+      nav:false,
+    },
+    1200:{
+      items:3,
+    }
+  }
 })
